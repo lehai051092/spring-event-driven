028eee228189b008e592b385b4c86d645171eebc
Le Hai <le.thanh.hai@sun-asterisk.com>
2024-09-25 15:01:15 +0700

    first commit

---
+++ src/main/java/example/springeventdrivent/SpringEventDriventApplication.java
@@ -0,0 +1,13 @@
+package example.springeventdrivent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventDriventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEventDriventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/springeventdrivent/SpringEventDriventApplicationTests.java
@@ -0,0 +1,13 @@
+package example.springeventdrivent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventDriventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
